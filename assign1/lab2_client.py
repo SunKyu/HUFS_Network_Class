477eab9b02e8c4aeaf6f556f032848ca5da1780a
SunKyu <leesunkeuy@naver.com>
2015-11-29 16:46:52 +0900

    merge optional excercise 1 and 2
    merge optional excercise 1 and 2
    so now we can check heartbeat and work like general pin app

--- assign1/lab2_client.py
+++ assign1/lab2_client.py
@@ -8,11 +8,14 @@ nr_recv = 0
 max_time = 0
 min_time = 10
 sum_time = 0
+BEAT_PERIOD = 3
 sock = socket(AF_INET, SOCK_DGRAM)
 sock.settimeout(1)
-for i in range(10):
+index = 0
+while True:
+    index +=1
     curtime =  time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-    mesg = "ping %d "%i + " " + curtime
+    mesg = "ping %d "%index + " " + curtime
     begin = time.time()
     sock.sendto(mesg , addr)
     try:
@@ -26,11 +29,14 @@ for i in range(10):
         if rtt_time < min_time:
             min_time = rtt_time
         print data + "RTT time = %f ms" % (rtt_time*1000) 
+        time.sleep(BEAT_PERIOD)
     except Exception as e:
         print "Request timed out"
+    except KeyboardInterrupt:
+        print "minimum %f ms, maximum %f ms, average %f ms " %(min_time*1000, max_time *1000, sum_time / float(nr_recv) * 1000)
+        pck_loss = (float(index-nr_recv))/float(index) * 100.0
+        print "packet loss rate is %d" %(pck_loss) + "%"
+        exit()
 
 sock.close()
-print "minimum %f ms, maximum %f ms, average %f ms " %(min_time*1000, max_time *1000, sum_time / float(nr_recv) * 1000)
-pck_loss = (10-nr_recv) * 10
-print "packet loss rate is %d" %(pck_loss) + "%"
 
