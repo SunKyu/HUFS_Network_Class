49e12c3563b926808e007503d1ea9c97f5ab2acc
SunKyu <leesunkeuy@naver.com>
2015-12-01 02:26:33 +0900

    add comment for report
    add comment for report

--- assign1/lab2_client.py
+++ assign1/lab2_client.py
@@ -4,15 +4,15 @@ import time
 udp_address = "127.0.0.1"
 udp_port = 12000
 addr = (udp_address, udp_port)
-nr_recv = 0
+nr_recv = 0 #the number of reciving message from server
 max_time = 0
 min_time = 10
 sum_time = 0
-BEAT_PERIOD = 3
+BEAT_PERIOD = 3 #period for heartbeat
 sock = socket(AF_INET, SOCK_DGRAM)
 sock.settimeout(1)
-index = 0
-count = 0
+index = 0 #sequnce num
+count = 0 #the count of message
 while True:
     count +=1
     index +=1
@@ -32,12 +32,12 @@ while True:
             min_time = rtt_time
         print data + "RTT time = %f ms" % (rtt_time*1000) 
         time.sleep(BEAT_PERIOD)
-    except Exception as e:
+    except Exception as e: #timeout, retry send message  
         print "Request timed out"
         print "retry send the message index %d"%index
         index -=1
         continue 
-    except KeyboardInterrupt:
+    except KeyboardInterrupt: #keyboard interrupt
         print "minimum %f ms, maximum %f ms, average %f ms " %(min_time*1000, max_time *1000, sum_time / float(nr_recv) * 1000)
         pck_loss = (float(count-nr_recv))/float(count) * 100.0
         print "packet loss rate is %d" %(pck_loss) + "%"
--- assign1/lab2_server.py
+++ assign1/lab2_server.py
@@ -5,25 +5,23 @@ import random
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', 12000))
 
-CHECK_TIME = 9
-max_index = 0
-min_index = 0 
+CHECK_TIME = 9 #heartbeat check time
 serverSocket.settimeout(CHECK_TIME)
-befotime = 0
-thistime = 0
+befotime = 0 #for time difference
+thistime = 0 #for time difference
 miss_check_time = time.time()
-miss_list = set()
+miss_list = set() #use set structre to check missing sequence of ping
 while True:
     rand = random.randint(0, 10)    
     try:
         message, address = serverSocket.recvfrom(1024)
-        if rand < 4: 
-            miss_list.add(int(message.split()[1]))
+        if rand < 4: #make loss
+            miss_list.add(int(message.split()[1])) #add sequence num to set
             continue
-        index = int(message.split()[1])
-        miss_list.discard(index)
+        index = int(message.split()[1]) #parsing sequnce num
+        miss_list.discard(index) #discard sequncenum from set
         message = message.upper()
-        if befotime:
+        if befotime: #calculate differnce
             befotime = thistime
         else:
             befotime = time.mktime(time.strptime(message.split('\t')[1], "%Y-%m-%d %H:%M:%S"))
@@ -33,13 +31,12 @@ while True:
         
         serverSocket.sendto(message, address)
     except timeout:
-        print e
         print "timeout, so cleint is dead"
         exit()
     except Exception as e:
         print e
         exit()
-    finally:
+    finally:#check periodic about loss packet
         if (time.time() - miss_check_time) > CHECK_TIME and len(miss_list)>0:
             print ("missing Ping numbers ")
             for i in range(len(miss_list)):
